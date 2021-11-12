fb81f77d3e963521d3464624839fabd2da6653ad
dinethpiyumantha <dinethpiyumantha@gmail.com>
2021-11-12 11:44:01 +0530

    config update

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,65 @@
+// Libraries
 import React from 'react'
+import GalleryGrid, { GalleryImage, GalleryImageList } from 'react-image-grid-gallary'
 
-import { ExampleComponent } from 'react-image-grid-gallary'
+// External stylesheets
 import 'react-image-grid-gallary/dist/index.css'
+import './styles.css'
 
+// Assets
+import ImageSample1 from './assets/1.jpg'
+import ImageSample2 from './assets/2.jpg'
+import ImageSample3 from './assets/3.jpg'
+import ImageSample4 from './assets/4.jpg'
+import ImageSample5 from './assets/5.jpg'
+import ImageSample6 from './assets/6.jpg'
+import ImageSample7 from './assets/7.jpg'
+import ImageSample8 from './assets/8.jpg'
+import ImageSample9 from './assets/9.jpg'
+import ImageSample10 from './assets/10.jpg'
+import ImageSample11 from './assets/11.jpg'
+import ImageSample12 from './assets/12.jpg'
+
+
+/**
+ * Main function component
+ * @returns JSX
+ */
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+    return (
+      <div>
+        {/* Without list */}
+        <h2>Using seperate elements</h2>
+        <GalleryGrid cols={5} hello="Hi">
+          <GalleryImage src={ ImageSample1 } title="1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, dolorem!" />
+          <GalleryImage src={ ImageSample2 } title="2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, dolorem!" />
+          <GalleryImage src={ ImageSample3 } title="3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, dolorem!" />
+          <GalleryImage src={ ImageSample4 } title="4 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, dolorem!" />
+          <GalleryImage src={ ImageSample5 } title="5 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, dolorem!" />
+          <GalleryImage src={ ImageSample6 } title="6 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, dolorem!" />
+          <GalleryImage src={ ImageSample7 } title="7 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, dolorem!" />
+          <GalleryImage src={ ImageSample8 } title="8 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, dolorem!" />
+          <GalleryImage src={ ImageSample9 } title="9 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, dolorem!" />
+          <GalleryImage src={ ImageSample10 } title="10 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, dolorem!" />
+          <GalleryImage src={ ImageSample11 } title="11 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, dolorem!" />
+          <GalleryImage src={ ImageSample12 } title="12 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, dolorem!" />
+        </GalleryGrid>
+
+        <br />
+
+        <h2>Using images as a list</h2>
+        {/* With list */}
+        <GalleryImageList 
+          cols={3}
+          imageList={[
+            {src: ImageSample1, title: "1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, dolorem!" },
+            {src: ImageSample2, title: "2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, dolorem!" },
+            {src: ImageSample3, title: "3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, dolorem!" },
+            {src: ImageSample4, title: "4 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, dolorem!" },
+          ]} 
+        />
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,177 @@
+// Libraies
 import React from 'react'
-import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+// External stylesheets
+import './styles.module.css'
+
+// Assets
+import ZoomIcon from 'zoom.svg'
+
+/**
+ * GalleryGrid class for cover child components
+ * It helps to create a image grid
+ */
+export default class GalleryGrid extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      cols: ''
+    }
+  }
+
+  render() {
+    // Create grid column style for css
+    let gridColumnStyle = ''
+    for (let i = 0; i < this.props.cols; i++) {
+      gridColumnStyle += 'auto '
+    }
+
+    // Calculate container width for grid
+    let gridContainerWidth = this.props.cols * 250
+
+    return (
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: gridColumnStyle,
+          width: gridContainerWidth + 'px'
+        }}
+      >
+        {/* Provide space to child componets in here */}
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+/**
+ * GalleryImage class is single image card
+ */
+export class GalleryImage extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      src: null,
+      title: '',
+      fullScreen: false
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState((prevState) => ({
+      fullScreen: !this.state.fullScreen
+    }))
+    console.log('You clicked ' + this.state.title)
+  }
+
+  render() {
+    return (
+      <div>
+        {/* Image Thumbnail card */}
+        <div
+          className='image-grid-card'
+          style={{ backgroundImage: 'url(' + this.props.src + ')' }} // Image source provide to here
+          onClick={this.handleClick}
+        >
+          {/* Image thumbnail title */}
+          <div className='image-grid-card-title'>
+            <p
+              style={{
+                textOverflow: 'ellipsis',
+                height: '20px',
+                margin: '0px',
+                width: '230px',
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                color: '#fff'
+              }}
+            >
+              {/* Image title render at here */}
+              { this.props.title }
+            </p>
+          </div>
+
+          {/* Background overlay animation */}
+          <div className='overflow-back'>
+            <img
+              src={ZoomIcon} // Zoom icon source provide to here
+              style={{
+                width: '50px',
+                transform: 'translate(-50%, -50%)',
+                marginTop: '75px',
+                marginLeft: '125px'
+              }}
+            />
+          </div>
+        </div>{' '}
+        {this.state.fullScreen && (
+          <div
+            style={{
+              backgroundColor: '#000000cc',
+              width: '100vw',
+              height: '100vh',
+              position: 'fixed',
+              transform: 'translate(-50%, -50%)',
+              left: '50%',
+              top: '50%',
+              zIndex: 99999
+            }}
+            className="fade-in"
+            onClick={this.handleClick}
+          >
+            <img
+              src={this.props.src}
+              style={{
+                height: '70vh',
+                position: 'fixed',
+                transform: 'translate(-50%, -50%)',
+                left: '50%',
+                top: '50%',
+                boxShadow: '0px 0px 20px 0px #000000'
+              }}
+            />
+            <small
+              style={{
+                color: '#fff',
+                position: 'fixed',
+                transform: 'translate(-50%, -50%)',
+                left: '50%',
+                bottom: 'calc(50% - 260px)',
+                backgroundColor: '#000000cc',
+                borderRadius: '100px',
+                padding: '5px 10px'
+              }}
+            >
+              {this.props.title}
+            </small>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+
+/**
+ * 
+ */
+export class GalleryImageList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <GalleryGrid cols={this.props.cols} >
+        {
+          this.props.imageList.map((image) => <GalleryImage src={image.src} title={image.title} />)
+        }
+      </GalleryGrid>
+    )
+  }
+
 }
